14c15c0819f702315278b21a8186bb8d1b71d7ab
RosarioChaves <rosariochaves111@gmail.com>
2024-04-17 04:55:03 -0300

    usuarios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session, redirect, url_for
 import mysql.connector
 
 from database import conectar_base_datos
@@ -7,6 +7,7 @@ from database import conectar_base_datos
 import os
 from werkzeug.utils import secure_filename 
 from math import ceil
+import bcrypt
 
 app = Flask(__name__)
 
@@ -36,7 +37,7 @@ def obtener_productos():
     cursor.close()
     return productos
 
-productos_por_pagina = 8
+productos_por_pagina = 9
 
 def obtener_productos_paginados(pagina):
     inicio = (pagina - 1) * productos_por_pagina
@@ -71,11 +72,84 @@ def mostrar_catalogo_categoria(categoria):
     productos = filtrar_categoria(categoria)
     return render_template('categoria.html', productos=productos, categoria=categoria)
 
+@app.route('/nuevo_usuario')
+def crear_usuario():
+    return render_template('f_nuevo_usuario.html')
+
+#cifrado de contraseñas
+def cifrar_contraseñas(contraseña):
+    hashed_password = bcrypt.hashpw(contraseña.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password
+
+#verificar contraseña cifrada
+def verificar_contraseña(contraseña, hashed_password):
+    return bcrypt.checkpw(contraseña.encode('utf-8'), hashed_password)
+
+@app.route('/cargar_usuario', methods=['POST'])
+def cargar_usuario():
+    if request.method == 'POST':
+        datos_usuario = request.form
+        # Cifrar la contraseña
+        hashed_password = cifrar_contraseñas(datos_usuario['contraseña'])
+        cursor = conexion.cursor()
+        cursor.execute("INSERT INTO usuario (nombre, apellido, email, contraseña) VALUES (%s, %s, %s, %s)",(datos_usuario['nombre'], datos_usuario['apellido'], datos_usuario['email'], hashed_password))
+        conexion.commit()
+        cursor.close()
+        return jsonify({"mensaje": "Cuenta creada"}), 200
+    else:
+        return jsonify({"error": "Método no permitido"}), 405
+
+@app.route('/verificar', methods=['GET', 'POST'])
+def verificar_usuario():
+    if request.method == 'POST':
+        email = request.form['email']
+        usuario = buscar_usuario(email)
+        if usuario:
+            return jsonify({"mensaje": "Usuario encontrado", "usuario": usuario}), 200
+        else:
+            return jsonify({"error": "Usuario no encontrado"}), 404
+    else:
+        return jsonify({"error": "Método no permitido"}), 405
+
+def buscar_usuario(email):
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM catalogo.usuario WHERE email = %s", (email,))
+    usuario = cursor.fetchone()
+    cursor.close() 
+    if usuario:
+        return {
+            'id': usuario[0], 
+            'nombre': usuario[1],  
+            'apellido': usuario[2], 
+            'email': usuario[3],       
+            'contraseña': usuario[4]   
+        }
+    else:
+        return None
+
+@app.route('/cuenta', methods=['GET', 'POST'])
+def acceso_cuentas():
+    if request.method == 'POST':
+        email = request.form['email']
+        contraseña = request.form['contraseña']
+        usuario = buscar_usuario(email)
+        if usuario and verificar_contraseña(contraseña, usuario['contraseña']):
+            session['usuario'] = usuario
+            return redirect(url_for('index'))
+        else:
+            return jsonify({"error": "Credenciales incorrectas"}), 401
+    else:
+        return render_template('f_acceso.html')
+    
+@app.route('/acceso')
+def render_acceso():
+    return render_template('acceso.html') 
+#######################################################
+
 # Ruta para cargar un nuevo producto
 @app.route('/formulario')
 def carga_producto():
     return render_template('formulario_carga_producto.html')
-
 @app.route('/cargar_producto', methods=['POST'])
 def cargar_producto():
     if request.method == 'POST':
--- catalogo_producto.sql
+++ catalogo_producto.sql
@@ -31,7 +31,7 @@ CREATE TABLE `producto` (
   `cantidad` int DEFAULT NULL,
   `foto` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-11  3:14:00
+-- Dump completed on 2024-04-17  4:52:01
